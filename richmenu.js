ddffb08da64d3771238f778e326e33e362e053cf
willkms <will_definite_choice@yahoo.co.jp>
2022-12-07 22:52:30 +0900

    環境変数追加

--- richmenu.js
+++ richmenu.js
@@ -1,7 +1,7 @@
 const line = require("@line/bot-sdk");
 
 const client = new line.Client({
-  channelAccessToken: "FNavCG/RFi8sY8eU4coOvdYvV/in1O1JRZL13OAxdo/b5W0RlgqM12hzFw6maJy9oBmhyL6TieCC7LqfhyKISB5fB7MNHm5O7UB/V5sh72XbF5ZkOWA+IDZhnlARzCp+xRqp8EKqBimJ/Ql1TN6W7wdB04t89/1O/w1cDnyilFU=",
+  channelAccessToken: process.env.ACCESS_TOKEN,
 });
 
-client.setDefaultRichMenu("richmenu-14ba1a60f9b789d8d39b0d9b7f37aee8");
+client.setDefaultRichMenu(process.env.RICHMENU_ID);
