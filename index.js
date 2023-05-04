85dcdbf79cfe3bfa99d96eee58bbd55de44ae5fb
willkms <will_definite_choice@yahoo.co.jp>
2023-05-04 11:49:00 +0900

    LIFF 2

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/confirm', function(req, res, next) {
   console.log(req.body)
   var input_name = req.body['name'];
   var input_date = req.body['date'];
-      res.render('confirm.html', 
+      res.render('confirm', 
           {
               name: input_name,
               date: input_date
